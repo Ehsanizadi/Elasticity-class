d92bd5f92cd9a8c51f2b778ad0e019e8fe4cccc2
Ehsanizadi <Ehsanizadi@outlook.com>
2014-06-08 21:16:30 +0200

    nano code cleaning

--- Elasticity.h
+++ Elasticity.h
@@ -1,16 +1,16 @@
 #ifndef Elasticity_h
 #define Elasticity_h
- 
+
 # include<iostream>
 
 // Class definition
 class linearElastic{
 	//private variables are the parameters which are hidden and non-accessible by the user (internal parameters)
-	private:	
+	private:
 	double E;
 	float v;
 
-	public: 
+	public:
 	//default definition
 	linearElastic(double elasticity, float poissonsRatio){
 		while (elasticity <0){
@@ -18,14 +18,14 @@ class linearElastic{
 			std::cin >> elasticity;
 		}
 		E = elasticity;
-	
+
 		while (poissonsRatio <0 || poissonsRatio>0.5){
 			std::cout << "Inalid value for Poission's Ratio!!\n Please try again!"<< std::endl;
 			std::cin >> poissonsRatio;
 		}
 		v = poissonsRatio;
 	}
-	
+
 	//alternate definition
 	linearElastic (double elasticity){
 		while (elasticity <0){
@@ -35,8 +35,8 @@ class linearElastic{
 		E = elasticity;
 		v = 0.3; // default value for poisson's ratio
 	}
-		
-	//weark form
+
+	//weak form
 	linearElastic(){
 		E = 30e6;
 		v= 0.3;
