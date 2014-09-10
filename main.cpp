603ec42c67d481936519e228cf8e08050b542fc1
Ehsanizadi <Ehsanizadi@outlook.com>
2014-09-10 09:05:45 +0200

    still debugging..

--- main.cpp
+++ main.cpp
@@ -29,22 +29,8 @@ int main () {
 	material1->setPoissonsRatio(0.1);
 	material2.setPoissonsRatio(0.5);
 	cout << "------------------------------------------------------------" << endl;
-	material1->getProperties();
-
-	linearElastic material1;
-	linearElastic material2(10e6);
-	linearElastic material3(uE, uP);
 	
-	material1.getProperties();
-	material2.getProperties();
-	material3.getProperties();
-
-	material1.setElasticity(1);
-	material1.setPoissonsRatio(0.1);
-	material2.setPoissonsRatio(0.5);
-	cout << "------------------------------------------------------------" << endl;
-	material1.getProperties();
-
+	material1->getProperties();
 	material2.getProperties();
 	material3.getProperties();
 	
@@ -59,16 +45,6 @@ int main () {
 	cout << "Poission's ratio for Material 3 is: " << material3.getPoissonsRatio() << endl;
 
 	delete material1; // the added class using pointer should be "de-allocated" from the memory heap
-
-
-	cout << "Elasticity for Material 1 is: " << material1.getElasticity() << endl;
-	cout << "Elasticity for Material 2 is: " << material2.getElasticity() << endl;
-	cout << "Elasticity for Material 3 is: " << material3.getElasticity() << endl;
-
-	cout << "Poission's ratio for Material 1 is: " << material1.getPoissonsRatio() << endl;
-	cout << "Poission's ratio for Material 2 is: " << material2.getPoissonsRatio() << endl;
-	cout << "Poission's ratio for Material 3 is: " << material3.getPoissonsRatio() << endl;
-
 
 	return 0;
 }
